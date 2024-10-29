2a26c894b95b40e125ff8e6d137a79e4a9342632
bernoussama <oussama.bernou@outlook.com>
2024-10-29 01:20:29 +0100

    update

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+Simple helper functions for pagination
+"""
+
+import csv
+from typing import Dict, List
+
+
+def index_range(page: int, page_size: int):
+    """
+    Return a tuple of size two containing start index and end index
+    """
+    return ((page - 1) * page_size, page * page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return a page of data"""
+        assert type(page) is int and type(page_size) is int
+        assert page > 0 and page_size > 0
+        try:
+            start, end = index_range(page, page_size)
+            return self.dataset()[start:end]
+        except IndexError:
+            return []
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Return a page of data with hypermedia"""
+        data = self.get_page(page, page_size)
+        total_pages = len(self.dataset()) / page_size
+        if len(self.dataset()) % page_size != 0:
+            total_pages += 1
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": page + 1 if page + 1 < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": int(total_pages),
+        }
