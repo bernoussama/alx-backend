0b74e4544a2b26454258797cc083ef72dc4706c1
bernoussama <oussama.bernou@outlook.com>
2024-10-30 22:53:50 +0100

    add docstring to methods

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -15,12 +15,14 @@ class BasicCache(BaseCaching):
         super().__init__()
 
     def put(self, key, item):
+        """Add an item in the cache"""
         if (key or item) is None:
             pass
         else:
             self.cache_data[key] = item
 
     def get(self, key):
+        """Get an item by key"""
         if key is None or key not in self.cache_data:
             return None
         else:
