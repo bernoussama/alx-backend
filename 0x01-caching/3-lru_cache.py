b55d405ea492774135b49e9e6bab7d7543cafe74
bernoussama <oussama.bernou@outlook.com>
2024-10-31 00:06:53 +0100

    lru cache

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" LIFO cache module
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LIFOCache(BaseCaching):
+    """basic cache defines:
+    - constants of your caching system
+    - where your data are stored (in a dictionary)
+    """
+
+    def __init__(self):
+        """Initiliaze"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
+        if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+            last, _ = self.cache_data.popitem(True)
+            print("DISCARD:", last)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=False)
+
+    def get(self, key):
+        """Get an item by key"""
+        # if key is None or key not in self.cache_data:
+        #     return None
+        # else:
+        if key not in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
