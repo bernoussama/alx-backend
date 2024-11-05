3b026d7747a5447e349cd9d39bf471abbfa7bfa8
bernoussama <oussama.bernou@outlook.com>
2024-11-05 20:06:59 +0100

    added babel

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,8 +2,20 @@
 """basic flask app"""
 
 from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+# init babel
+babel = Babel(app)
 
 
 @app.route("/")
