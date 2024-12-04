8577bd7cf2c3f6b283d7e0b9c1603112ab38a5e1
bernoussama <oussama.bernou@outlook.com>
2024-12-05 00:53:35 +0100

    done

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,11 @@
+const { createClient } = require("redis");
+
+const client = createClient();
+
+client.on("error", (err) => {
+  console.log("Redis client not connected to the server:", err.toString());
+});
+
+client.on("connect", () => {
+  console.log("Redis client connected to the server");
+});
